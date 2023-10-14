c4d880e0aa439f903d68106283a859fe412428a8
Vasilii Leontev <leovas1972@gmail.com>
2023-10-14 07:56:28 +0700

    modified test_registration_form

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -9,13 +9,13 @@ def test_successful(setup_chrome):
     first_name = "Alex"
     last_name = "Egorov"
 
-    with allure.step("Open registrations form"):
+    with allure.step("Открыть форму регистрации"):
         browser.open("https://demoqa.com/automation-practice-form")
         browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
         browser.driver.execute_script("$('footer').remove()")
         browser.driver.execute_script("$('#fixedban').remove()")
 
-    with allure.step("Fill form"):
+    with allure.step("Заполнить форму"):
         browser.element("#firstName").set_value(first_name)
         browser.element("#lastName").set_value(last_name)
         browser.element("#userEmail").set_value("alex@egorov.com")
@@ -36,7 +36,7 @@ def test_successful(setup_chrome):
         browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
         browser.element("#submit").click()
 
-    with allure.step("Check form results"):
+    with allure.step("Проверить результаты формы"):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
         # browser.element(".table-responsive").should(
         #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
